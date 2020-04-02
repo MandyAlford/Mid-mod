33deccde881d43aa1d6feb9fb6b8a7546029a395
Mandy Alford <mandyalford@gmail.com>
2020-04-02 10:15:23 -0600

    Get reservation info to load on DOM

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
-const ReservationCard = () => {
+const ReservationCard = (props) => {
   return(
-    <p>I am a reservation</p>
+    <div className='reservation-card'>
+    <h1 id='card-name'>{props.name}</h1>
+     <h2 className='card-details'>{props.date}</h2>
+     <h2 className='card-details'>{props.time} pm</h2>
+     <h2 className='card-details'>Number of Guests: {props.number}</h2>
+    </div>
   )
 }
 
